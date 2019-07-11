3de99c9d243dfed01825c1b8e93cd7bfa4c98daa
user <user@192.168.201.236>
2019-07-11 10:48:36 +0900

    dependency 삭제
    aop 설정 삭제

--- src/main/java/com/orz/OrzApplication.java
+++ src/main/java/com/orz/OrzApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
 @ComponentScan("com.orz")
 @SpringBootApplication
 public class OrzApplication {
