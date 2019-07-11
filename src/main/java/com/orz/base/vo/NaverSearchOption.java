8bad2fc3d328b7404886534d7b1a2c87815e3756
user <user@192.168.201.236>
2019-07-11 11:05:39 +0900

    주석추가

--- src/main/java/com/orz/base/vo/NaverSearchOption.java
+++ src/main/java/com/orz/base/vo/NaverSearchOption.java
@@ -9,7 +9,7 @@ public class NaverSearchOption implements Serializable {
 
 	private static final long serialVersionUID = -2316570866081739396L;
 	
-	private String item; //상품명   
+	private String item; //상품명    
 	private String delevery; // 무료배송여부 1(무료배송)
 	private String useYn; // 상품 사용여부
 	private String sort; // rel(랭킹순) price_asc(최저가순) review(리뷰많은순)
